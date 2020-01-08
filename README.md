75a2f607cf15661239ea5a0fa8d99559acc9b20d
Olivia Guest <guest.olivia@gmail.com>
2020-01-08 14:38:53 +0200

    script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CFD-backgrounds
+# CFD backgrounds stimuli generator
+
+Please fill in the directories in `directories.py` — and then to run:
+```
+python create_stimuli.py
+```
+
+Once the script has run, the stimuli will be in the directory you have chosen.
---
+++ directories.py
@@ -0,0 +1,9 @@
+
+# Where the faces for the foreground are saved:
+fg_dir = '/home/olivia/Data/test_faces/'
+# Where the backgrounds are saved:
+bg_dir = '/home/olivia/Data/Backgrounds/'
+# Where the cropped backgrounds will be saved:
+cropped_dir = '/home/olivia/Data/Backgrounds/Cropped/'
+# Where the final stimuli will be saved (foreground on background):
+stimuli_dir = '/home/olivia/Data/Stimuli/'
