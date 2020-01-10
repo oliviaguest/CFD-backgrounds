2aacc3fc9fa8a1a7b8085cba63482cff55056975
oliviaguest <guest.olivia@gmail.com>
2020-01-10 09:57:29 +0200

    update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# CFD backgrounds stimuli generator
+# create-stimuli
 
-Please fill in the directories in `directories.py` — and then to run:
+Please fill in the directories in `directories.py`.
+
+To run with a ratio of, e.g., 0.17 send the `-r` flag followed by `0.17`:
 ```
-python create_stimuli.py
+python create_stimuli.py -r 0.17
 ```
 
-Once the script has run, the stimuli will be in the directory you have chosen.
+Once the script has run, the stimuli will be in the directory you have chosen along with a CSV file: `stimuli.csv`, which contains useful details per stimulus. 
--- create_stimuli.py
+++ create_stimuli.py
@@ -5,38 +5,27 @@ from PIL import Image, ImageChops
 import numpy as np
 import pandas as pd
 import ntpath
-
+import argparse
 from directories import bg_dir, fg_dir, stimuli_dir, cropped_dir
-
+from utils import get_pixel_ratio
 
 def trim(im):
     """Takes an image object and trims all the contiguous space around the
     edges."""
-    # https://stackoverflow.com/a/48605963/3433914
+    # From: https://stackoverflow.com/a/48605963/3433914
     bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
     diff = ImageChops.difference(im, bg)
     diff = ImageChops.add(diff, diff, 2.0, -100)
-
+    # Bounding box given as a 4-tuple defining the left, upper, right, and
+    # lower pixel coordinates.
     # If the image is completely empty, this method returns None.
     bbox = diff.getbbox()
     if bbox:
         return im.crop(bbox)
 
 
-def pixel_ratio(im):
-    """Takes an image object and returns the ratio of the number of pixels that
-    have exactly 255 in the alpha channel and all the pixels."""
-    pixels = list(im.getdata())
-    count = 0
-    # Count the non-transparent pixels
-    for i, pixel in enumerate(pixels):
-        # if pixel[3] > 128:
-        if pixel[3] == 255:
-            count += 1
-    return count / len(pixels)
 
-
-def crop_backgrounds(bg_dir, cropped_dir, cropped='both'):
+def crop_backgrounds(bg_dir, cropped_dir):
     """Normalise the backgrounds so they have the same size by cropping and
     resizing."""
     widths = []
@@ -68,53 +57,82 @@ def crop_backgrounds(bg_dir, cropped_dir, cropped='both'):
         except OSError:
             pass
 
-        if cropped == 'bottom':
-            # Cropping from the bottom
-            im_bottom = im.crop(
-                (0, 0, im.width, im.height - crop_pixels))
-            im_bottom.save(cropped_dir + ntpath.basename(file) +
-                           '_cropped_bottom' + '.jpg')
-            return (im_bottom.height, im_bottom.width)
+        # Cropping from the bottom
+        im_bottom = im.crop(
+            (0, 0, im.width, im.height - crop_pixels))
+        im_bottom.save(cropped_dir + ntpath.basename(file) +
+                       '_cropped_bottom' + '.jpg')
+
+        # Cropping from the top
+        im_top = im.crop((0,  crop_pixels, im.width, im.height))
+        im_top.save(cropped_dir + ntpath.basename(file) +
+                    '_cropped_top' + '.jpg')
+
+        # Cropping from both
+        im_both = im.crop((0, crop_pixels_both, im.width, im.height -
+                           crop_pixels_both))
+        # This is here to stop any rounding errors with resizing.
+        im_top = im_both.resize((im_top.width, im_top.height))
+        im_both.save(cropped_dir + ntpath.basename(file) +
+                     '_cropped_both' + '.jpg')
+    return (im_top.height, im_top.width)
 
 
-        if cropped == 'top':
-            # Cropping from the top
-            im_top = im.crop((0,  crop_pixels, im.width, im.height))
-            im_top.save(cropped_dir + ntpath.basename(file) +
-                        '_cropped_top' + '.jpg')
-            return (im_top.height, im_top.width)
-
-        if cropped == 'both':
-            # Cropping from both
-            im_both = im.crop((0, crop_pixels_both, im.width, im.height -
-                               crop_pixels_both))
-            # This is here to stop any rounding errors with resizing
-            im_both = im_both.resize((im_both.width, im_both.height))
-            im_both.save(cropped_dir + ntpath.basename(file) +
-                         '_cropped_both' + '.jpg')
-            return (im_both.height, im_both.width)
-
-
-def paste_image_on_background(fg_im, bg_im):
+def paste_image_on_background(fg_im, bg_im, pixel_ratio=None):
     """Takes a foreground image, places it on top of a background image, and
     returns the combined image and the pixel ratio of foreground to
     background."""
     fg_im = trim(fg_im)
-
     fg_im.thumbnail((bg_im.width, bg_im.height))
 
-    pixels_to_center = int(np.round(bg_im.width - fg_im.width) / 2)
-    fg_im = fg_im.crop(
-        (-pixels_to_center, 0, bg_im.width - pixels_to_center, bg_im.height))
+    # If a pixel ratio is given, then we resize the foreground image.
+    if pixel_ratio:
+
+        pixels_to_center_x = int(np.round(bg_im.width - fg_im.width) / 2)
+        pixels_to_bottom = bg_im.height - fg_im.height
+
+        test_im = fg_im.crop((-pixels_to_center_x,
+                              -pixels_to_bottom,
+                              bg_im.width - pixels_to_center_x,
+                              bg_im.height - pixels_to_bottom))
+        # We need to know the current pixel ratio before we make the foreground
+        # even smaller.
+        current_pixel_ratio = get_pixel_ratio(test_im)
+
+        # This is the number we need to get he desired ratio.
+        diff_pixel_ratio = pixel_ratio / current_pixel_ratio
+
+        # Check this ratio makes sense.
+        if pixel_ratio > current_pixel_ratio:
+            print('This pixel ratio means that the face is taller/wider than '
+                  'the background.')
+            exit()
+
+        # Apply the transformation.
+        fg_dims = (int(np.round((np.sqrt(diff_pixel_ratio) * fg_im.width))),
+                   int(np.round((np.sqrt(diff_pixel_ratio) * fg_im.height))))
+        fg_im = fg_im.resize(fg_dims)
+
+    pixels_to_center_x = int(np.round(bg_im.width - fg_im.width) / 2)
+    pixels_to_bottom = bg_im.height - fg_im.height
+
+    # Calculate the placement of the foreground on the background.
+    fg_im = fg_im.crop((-pixels_to_center_x,
+                        -pixels_to_bottom,
+                        bg_im.width - pixels_to_center_x,
+                        bg_im.height - pixels_to_bottom))
     assert fg_im.height == bg_im.height
     assert fg_im.width == bg_im.width
+
+    # And finally, paste one image onto the other.
     bg_im.paste(fg_im, (0, 0), fg_im)
 
-    return bg_im, pixel_ratio(fg_im)
+    return bg_im, get_pixel_ratio(fg_im)
 
 
-if __name__ == "__main__":
+def main(desired_ratio):
 
+    # Normalise the backgrounds.
     crop_backgrounds(bg_dir, cropped_dir)
 
     df = pd.DataFrame(
@@ -124,14 +142,16 @@ if __name__ == "__main__":
     for fg_infile in glob.glob(fg_dir + "*.png"):
         for bg_infile in glob.glob(cropped_dir + "*.jp*g"):
             bg_file, ext = os.path.splitext(bg_infile)
+
             bg_im = Image.open(bg_infile).convert('RGBA')
 
             fg_file, ext = os.path.splitext(fg_infile)
             fg_im = Image.open(fg_infile).convert('RGBA')
 
-            stim_im, ratio = paste_image_on_background(fg_im, bg_im)
-            # print('Pixel ratio of face to background = ', ratio)
-            # Directory to savr the output stimuli.
+            stim_im, ratio = paste_image_on_background(fg_im, bg_im,
+                                                       desired_ratio)
+
+            # Directory to save the output stimuli.
             save_dir = stimuli_dir + ntpath.basename(bg_file) + '/'
             try:
                 os.makedirs(save_dir)
@@ -149,3 +169,14 @@ if __name__ == "__main__":
             print('Saving:', save_path)
             stim_im.convert('RGB').save(save_path)
     df.reset_index(drop=True).to_csv(stimuli_dir + 'stimuli.csv')
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Create the stimuli.')
+    parser.add_argument("-r",
+                        help="Desired pixel ratio for foreground/background.",
+                        type=float, default='0.25')
+
+    args = parser.parse_args()
+    main(args.r)
